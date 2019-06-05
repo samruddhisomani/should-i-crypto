122010c7d4e63a4b62374b6a91dff1ab1a201e70
Samruddhi Somani <ssomani@homeaway.com>
2019-06-05 14:53:03 -0500

    updates to make it responsive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
+let VIEW;
+
 fetch("chart.vl.json")
   .then(function(response) {
     return response.json();
   })
   .then(function(spec) {
-    vegaEmbed(".chart", spec);
+    vegaEmbed(".chart", spec)
+      .then(function(result) {
+        // result.view is the Vega View, vlSpec is the original Vega-Lite specification
+        VIEW = result.view;
+        const x = document.querySelector(".chart");
+        VIEW.width(x.clientWidth)
+          .height(x.clientHeight)
+          .run();
+      })
+      .catch(console.error);
   });
+
+// resize code from
+// view-source:https://streeteasy-market-data-api.s3.amazonaws.com/vis/v3.1/js/main.js
+window.addEventListener("resize", () => {
+  const x = document.querySelector(".chart");
+  VIEW.width(x.clientWidth)
+    .height(x.clientHeight)
+    .run();
+});
