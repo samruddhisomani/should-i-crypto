ff8553d0bca63d7f9eabd721ed823d8511d9fe97
Samruddhi Somani <ssomani@homeaway.com>
2019-06-09 12:20:39 -0500

    font family on buttons

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
 let VIEW;
 
-fetch("chart.vl.json")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(spec) {
-    vegaEmbed(".chart", spec)
-      .then(function(result) {
-        // result.view is the Vega View, vlSpec is the original Vega-Lite specification
-        VIEW = result.view;
-        const x = document.querySelector(".chart");
-        VIEW.width(x.clientWidth)
-          .height(x.clientHeight)
-          .run();
-      })
-      .catch(console.error);
-  });
+const formSubmit = () => {
+  fetch("chart.vl.json", { mode: "cors" })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(spec) {
+      vegaEmbed(".chart", spec)
+        .then(function(result) {
+          // result.view is the Vega View, vlSpec is the original Vega-Lite specification
+          VIEW = result.view;
+          const x = document.querySelector(".chart");
+          VIEW.width(x.clientWidth)
+            .height(x.clientHeight)
+            .run();
+        })
+        .catch(console.error);
+    });
+};
 
 // resize code from
 // view-source:https://streeteasy-market-data-api.s3.amazonaws.com/vis/v3.1/js/main.js
