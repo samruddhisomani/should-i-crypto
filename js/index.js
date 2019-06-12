98a565950ee7d0d5e2a9a5ecd1339a02899ccba5
Patrick Hardin <phardin@homeaway.com>
2019-06-12 00:29:24 -0500

    Unfinished business

--- js/index.js
+++ js/index.js
@@ -1,13 +1,71 @@
+const ShouldICrypto = {
+  getCrypto: function (coin) {
+    const baseUrl = 'https://api.coingecko.com/api/v3/';
+    const path = 'coins/' + coin + '/market_chart/range';
+    const from = moment().subtract(7, 'days').unix();
+    const to = moment().unix();
+    const params = '?vs_currency=gbp&from=' + from + '&to=' + to;
+    return axios.get(baseUrl + path + params);
+  },
+  getFiat: function (currency) {
+    const baseUrl = 'https://api.exchangeratesapi.io/';
+    const path = 'history';
+    const startAt = moment().subtract(7, 'days').format('YYYY-MM-DD');
+    const endAt = moment().format('YYYY-MM-DD');
+    const params = '?base=GBP' + '&start_at=' + startAt + '&end_at=' + endAt + "&symbols=" + currency;
+    return axios.get(baseUrl + path + params);
+  },
+  getData: function (coin, currency) {
+    return axios.all([this.getCrypto(coin), this.getFiat(currency)]);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  ShouldICrypto.getData('bitcoin', 'USD').then(function (response) {
+    let crypto = response[0].data.prices;
+    let fiat = response[1].data.rates;
+    let cryptoParsed = crypto.map(function (a) {
+      return {
+        x: a[0],
+        y: a[1]
+      };
+    });
+    let fiatParsed = Object.keys(fiat).map(function (b) {
+      return {
+        x: moment(b).unix(),
+        y: fiat[b].USD
+      }
+    })
+
+    console.log(cryptoParsed);
+    console.log(fiatParsed);
+
+    var graph = new Rickshaw.Graph({
+      element: document.querySelector('#graph'),
+      series: [
+        {
+          color: 'red',
+          data: cryptoParsed
+        }, {
+          color: 'blue',
+          data: fiatParsed
+        }
+      ]
+    });
+    graph.render();
+  })
+})
+
 let VIEW;
 
 const formSubmit = () => {
   fetch("chart.vl.json", { mode: "cors" })
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(spec) {
+    .then(function (spec) {
       vegaEmbed(".chart", spec)
-        .then(function(result) {
+        .then(function (result) {
           // result.view is the Vega View, vlSpec is the original Vega-Lite specification
           VIEW = result.view;
           const x = document.querySelector(".chart");
